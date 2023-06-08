60da0095c70ba6fceaf0422709b2a5ec10045c64
Cole <cnemec@student.austintrinity.org>
2023-06-07 20:22:43 -0700

    Integrated edge_node and vertex into my_graph files & implemented edge_node & vertex methods

--- my_graph.cc
+++ my_graph.cc
@@ -1,2 +1,52 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
 #include "my_graph.h"
+
+void vertex::display() {
+	std::cout << "VERTEX: " << std::endl
+	<< "  data: \"" << data <<"\""<< std::endl
+	<< "  edge list:" << std::endl;
+	for(edge_node e: edge_list){
+		e.display();
+	}
+}
+
+void edge_node::display() {
+	std::cout << "    EDGE: (\"" << edge.first.data << "\", \"" << edge.second.data << "\")" << std::endl;
+}
+
+
+
+
+
+// -----------------------
+// my_graph Implementation
+// -----------------------
+
+// ---------------------------
+// Constructors and Destructor
+// ---------------------------
+
+my_graph::my_graph() {} // TODO
+
+my_graph::my_graph(int size_) {} // TODO
+
+my_graph::my_graph(std::vector<vertex_data_t> data_vec_){} // TODO
+
+my_graph::~my_graph() {} // TODO
+
+
+
+void my_graph::display() const {
+	std::cout << "Displaying graph in order of adjacency_list entries:" << std::endl;
+	for(auto v : adjacency_list){
+		v.display();
+	}
+}
+
+bool my_graph::addVertex(vertex_data_t d_){ // Adds vertex at end of adjacency_list // TODO what if a vertex with the same vertex_data_t exists?
+	vertex v(d_);
+	adjacency_list.push_back(v);
+}
--- my_graph.h
+++ my_graph.h
@@ -1,32 +1,87 @@
 // Represents the full graph ADT
+	// Un-weighted, directed graph.
+		// Graph direction is represented by order of edge in edge_node.edge
 
 #ifndef CS163_PROG5_MY_GRAPH_H
 #define CS163_PROG5_MY_GRAPH_H
 
 #include <vector>
-#include "vertex.h"
-#include "GraphInterface.h"
-#include "edge_node.h"
+#include <string>
+#include <utility>
 
+// TODO:
+	// TODO Finish implementing my_graph!   
+	// TODO Determine whether struct datas should be * or not
+	// TODO Determine whether to make my_graph::add_vertex/remove_vertex have arguments be & or not
+	// TODO Can I use std::pair from <utility>?
+	//      If not, just use a vector and enforce its size = 2:P
 
-class my_graph: GraphInterface  {
-private:
-	std::vector<vertex> adjacency_list; // an adj list of vertexes with their own edge list
+using vertex_data_t = std::string;
 
-public:
-	int getNumVertices() const;
-	int getNumEdges() const;
+struct vertex{ // TODO since my_graph's adjacency_list is a std::vector<vertex>, will resizing that & invoking the vertex copy constructor cause issues here?
+	std::vector<struct edge_node> edge_list; // TODO should this be a *?
+	vertex_data_t data; // TODO Should this be a * or not?
 
-	bool add(LabelType start, LabelType end, int edgeWeight);
-	bool remove(LabelType start, LabelType end);
-
-	int getEdgeWeight(LabelType start, LabelType end) const;
-
-	void depthFirstTraversal(LabelType start, void visit(LabelType&));
-
-	void breadthFirstTraversal(LabelType start, void visit(LabelType&));
+	explicit vertex(vertex_data_t& d_):data(d_){} // TODO should we enforce "must initialize vertex with a vertex_data_t? Does this do that? // TODO is explicit useful here?
 
 	void display();
+};
+
+struct edge_node{
+	std::pair<vertex, vertex>  edge; // TODO should this be a *? // Directional: first vertex is source of edge, second vertex is target of edge.
+
+	explicit edge_node(std::pair<vertex, vertex> e_): edge(e_){}
+	edge_node(vertex a, vertex b): edge(std::make_pair(a, b)){} // TODO double check!
+
+	void display();
+};
+
+class my_graph {
+
+public:
+	// ---------------------------
+	// Constructors and Destructor
+	// ---------------------------
+	my_graph();
+	my_graph(int size_); // Create a graph of size size_, where each vertex has data "null" and an empty edge_list
+	my_graph(std::vector<vertex_data_t> data_vec_); // Turn a vector of vertex_data_t into a graph of vertexes containing that data with empty edge lists
+	~my_graph();
+
+	void display() const;
+
+//	int getNumVertices() const; // TODO necessary?
+	virtual int getNumVertices() const = 0; // TODO necessary?
+	virtual int getNumEdges() const = 0; // TODO necessary?
+
+	// -------------------------
+	// Add/Remove Parts of Graph
+	// -------------------------
+		// TODO can add/remove vertex/edge methods be made const?
+	bool addVertex(vertex_data_t d_) ;
+	bool removeVertex(vertex_data_t d_);
+
+	bool addEdge(vertex_data_t * start_, vertex_data_t * end_) ;
+	bool removeEdge(vertex_data_t * start_, vertex_data_t * end_) ;
+
+	// ------------------------
+	// Public Traversal Methods
+	// ------------------------
+	void depthFirstTraverse(vertex_data_t start, void visit(vertex_data_t&)); // Start a  DFT at the vertex with data == start
+
+	void breadthFirstTraverse(vertex_data_t start, void visit(vertex_data_t&)); // Start a  BFT at the vertex with data == start
+
+private:
+	// ----
+	// Data
+	// ----
+	std::vector<vertex> adjacency_list; // an adj list of vertexes with their own edge list
+
+	// ----------------------------------
+	// Recursive Traversal Helper Methods
+	// ----------------------------------
+	void recursive_BreadthFirstTraverse(vertex & v_, void visit(vertex_data_t&));
+	void recursive_DepthFirstTraverse(vertex & v_, void visit(vertex_data_t&));
+
 };
 
 
